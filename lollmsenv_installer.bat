8cbdf747e873841cf8620db741f7d323bd87b960
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2024-10-01 01:41:12 +0200

    Update lollmsenv_installer.bat

--- lollmsenv_installer.bat
+++ lollmsenv_installer.bat
@@ -1,15 +1,18 @@
 @echo off
+:: Version number
+set VERSION=1.2
+
 :: URL of the latest release
-set RELEASE_URL=https://github.com/ParisNeo/LollmsEnv/archive/refs/tags/V1.0.zip
+set RELEASE_URL=https://github.com/ParisNeo/LollmsEnv/archive/refs/tags/V%VERSION%.zip
 
 :: Temporary directory for downloading and extraction
-set TEMP_DIR=%TEMP%\lollmsenv_install
+set TEMP_DIR=.\lollmsenv_install
 
 :: Create temporary directory
 if not exist "%TEMP_DIR%" mkdir "%TEMP_DIR%"
 
 :: Download the latest release
-echo Downloading LollmsEnv...
+echo Downloading LollmsEnv version %VERSION%...
 powershell -Command "Invoke-WebRequest -Uri '%RELEASE_URL%' -OutFile '%TEMP_DIR%\lollmsenv.zip'"
 
 :: Extract the archive
@@ -17,7 +20,7 @@ echo Extracting files...
 powershell -Command "Expand-Archive -Path '%TEMP_DIR%\lollmsenv.zip' -DestinationPath '%TEMP_DIR%' -Force"
 
 :: Change to the extracted directory
-cd /d "%TEMP_DIR%\LollmsEnv-1.0"
+cd /d "%TEMP_DIR%\LollmsEnv-%VERSION%"
 
 :: Run the install script with forwarded parameters
 echo Running installation...
@@ -28,4 +31,4 @@ echo Cleaning up...
 cd /d "%USERPROFILE%"
 rmdir /s /q "%TEMP_DIR%"
 
-echo Installation complete.
+echo Installation of LollmsEnv version %VERSION% complete.
