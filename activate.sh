309a75cc30b029b30b9e144ab108b7bd1165b3f2
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2024-10-01 14:00:41 +0200

    Update activate.sh

--- activate.sh
+++ activate.sh
@@ -5,7 +5,7 @@
 # Built by ParisNeo using Lollms
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 if [ -d "$SCRIPT_DIR/.lollmsenv" ]; then
-    export LOLLMSENV_DIR="$SCRIPT_DIR/.lollmsenv"
+    export LOLLMSENV_DIR="$SCRIPT_DIR/.lollmsenv/bin"
 else
     export LOLLMSENV_DIR="$HOME/.lollmsenv/bin"
 fi
