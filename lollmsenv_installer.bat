163bc5f803d648d0cab1f97d339626c870994dbd
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2024-10-07 12:07:09 +0200

    Update lollmsenv_installer.bat

--- lollmsenv_installer.bat
+++ lollmsenv_installer.bat
@@ -1,6 +1,6 @@
 @echo off
 :: Version number
-set VERSION=1.2.9
+set VERSION=1.2.10
 
 :: URL of the latest release
 set RELEASE_URL=https://github.com/ParisNeo/LollmsEnv/archive/refs/tags/V%VERSION%.zip
